d856ef828f952059ee2dac6dc48a4f9d69bb3200
landeilsonveloso <landeilsonveloso@gmail.com>
2025-06-16 14:57:49 -0300

    ajustes

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -71,7 +71,6 @@ export default function DashboardPage() {
                             }
                         }}
                     />
-
                 </Div>
                 
                 <Table
